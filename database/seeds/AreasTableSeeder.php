4453339c115230f7ae729c22f519e23739976dc2
refresh-mintowa <towagarrix@gmail.com>
2021-04-16 18:56:37 +0900

    areaSeederの中身変更

--- database/seeds/AreasTableSeeder.php
+++ database/seeds/AreasTableSeeder.php
@@ -11,11 +11,16 @@ class AreasTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('areas')->insert(['name'=>'北海道・東北']);
-        DB::table('areas')->insert(['name'=>'関東']);
-        DB::table('areas')->insert(['name'=>'中部']);
-        DB::table('areas')->insert(['name'=>'近畿']);
-        DB::table('areas')->insert(['name'=>'中国・四国']);
-        DB::table('areas')->insert(['name'=>'九州']);
+       
+        DB::table('areas')->insert(['id' => 1, 'name'=>'北海道・東北']);
+        DB::table('areas')->insert(['id' => 2,'name'=>'関東']);
+        DB::table('areas')->insert(['id' => 3,'name'=>'中部']);
+        DB::table('areas')->insert(['id' => 4,'name'=>'近畿']);
+        DB::table('areas')->insert(['id' => 5,'name'=>'中国・四国']);
+        DB::table('areas')->insert(['id' => 6,'name'=>'九州']);
+          DB::table('areas')->insert([
+            'id' => 7,
+            'name'=>'食事'
+            ]);
     }
 }
